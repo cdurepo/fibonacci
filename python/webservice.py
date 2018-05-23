5921c67b800f311a1e1713e82d4f6257f9840452
Chris Durepo <chris.durepo@gmail.com>
2018-05-23 00:48:32 -0400

    Created a docker-compose to add a redis server. Now we have a local array with some values and a redis server that has higher numbers.

--- python/webservice.py
+++ python/webservice.py
@@ -5,21 +5,47 @@
 # Created by Chris Durepo May 22 2018
 #######################
 import json
+import redis
 from flask import Flask
 from flask import request
 from flask import jsonify
 
+### Setup
+max_array=1000
+max_redis=10000
+#Connect to redis
+
+rdb = redis.Redis(
+    host='172.18.0.2',
+    port="6379"
+)
+print "testing rdb"
+rdb.set('test', 'pass')
+value = rdb.get('test')
+print "Value:",value
 
 # Function to fill array
 def populate_fib_array ():
     print "Populating array"
     count=2
     fib_array=[1,1]
-    while count < 10000:
+    while count < max_array:
         fib_array.append(fib_array[count-2]+fib_array[count-1])
         #print "fib array count is:",fib_array[count]
         count+=1
     return fib_array
+
+def populate_fib_redis (fib_one, fib_two):
+    print "Populating redis"
+    count=max_array+1
+    while count < max_redis:
+        value=fib_one+fib_two
+        rdb.set(count,value)
+        count+=1
+        fib_one=fib_two
+        fib_two=value
+    return
+
 def create_output (length, fib_array):
     #This is the function that will walk the array and return the array
     #for the given position
@@ -29,7 +55,17 @@ print "starting service"
 fib_array=populate_fib_array()
 print "Done filling array"
 #print "10:",create_output(10000, fib_array)
-
+print "Start filling redis"
+fib_one=fib_array[-2]
+fib_two=fib_array[-1]
+print fib_array[-1]
+print fib_array[-2]
+orig=fib_array[-1]+fib_array[-2]
+print "should be:",orig
+populate_fib_redis(fib_one, fib_two)
+print "verify"
+verify=rdb.get(1001)
+print "fib(1001)",verify
 #Building the webservice app
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -40,4 +76,4 @@ def hello_world():
     if (length > 0):
         return  jsonify(response="success",output=create_output(length, fib_array))
     else:
-        return "There was an error"
+        return jsonify(response="error",message="invalid reqeust")
