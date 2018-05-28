80195c293ab0b342265c1a36f6045cdf54bad245
Chris Durepo <chris.durepo@gmail.com>
2018-05-28 15:50:21 -0400

    Got tornado working.  I am able to return error messages. However, read and writing files is different so I have to get that figured out.

--- python/webservice.py
+++ python/webservice.py
@@ -8,9 +8,11 @@ import json
 import redis
 import threading
 import os
+import tornado.httpserver
 import tornado.escape
 import tornado.ioloop
 import tornado.web
+from tornado import gen
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -73,29 +75,29 @@ def populate_fib_array ():
     return
 
 
-def create_output (length):
-    #This is the function that will walk the array and return the array
-    #for the given position
-    global local_high
-    print "local_high:",local_high
-    if length <= local_high:
-        fib_read.seek(0)
-        fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
-    else:
-        # If the length is more than we have in the cache file_name
-        # We need to update the cache file and then return the whole thing.
-        while (local_high < length):
-            fib_file.write((','+rdb.get(local_high+1)))
-            rdb.set('index_'+str(local_high+1),fib_file.tell())
-            local_high+=1
-
-        fib_file.flush()
-        fib_read.seek(0)
-        fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
-        #fib_output=fib_read.read()
-        #fib_output.extend(rdb.mget(range(max_array+1,length+1,1)))
-    message=fib_output
-    return
+# def create_output (length):
+#     #This is the function that will walk the array and return the array
+#     #for the given position
+#     global local_high
+#     print "local_high:",local_high
+#     if length <= local_high:
+#         fib_read.seek(0)
+#         fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
+#     else:
+#         # If the length is more than we have in the cache file_name
+#         # We need to update the cache file and then return the whole thing.
+#         while (local_high < length):
+#             fib_file.write((','+rdb.get(local_high+1)))
+#             rdb.set('index_'+str(local_high+1),fib_file.tell())
+#             local_high+=1
+#
+#         fib_file.flush()
+#         fib_read.seek(0)
+#         fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
+#         #fib_output=fib_read.read()
+#         #fib_output.extend(rdb.mget(range(max_array+1,length+1,1)))
+#     message=fib_output
+#     return
 
 
 ########### Main #################
@@ -108,26 +110,90 @@ print "Done filling array"
 # app.config['JSON_SORT_KEYS'] = False
 #
 # @app.route("/")
-def hello_world(length):
-    try:
-         length = int(length)
-    except:
-        return jsonify(response="error", message="invalid reqeust, reqeust must be an integer")
-
-    if (length > 0):
-        if ((length <= local_high) or ((length > local_high) and (rdb.exists(length)))):
-            #print "current high",high
-            return  jsonify(response="success",output=create_output(length))
-        else:
-            return jsonify(response="error", message="Not calculated yet please try again later")
-    else:
-            return jsonify(response="error",message="invalid reqeust")
+# def hello_world(length):
+#     try:
+#          length = int(length)
+#     except:
+#         return jsonify(response="error", message="invalid reqeust, reqeust must be an integer")
+#
+#     if (length > 0):
+#         if ((length <= local_high) or ((length > local_high) and (rdb.exists(length)))):
+#             #print "current high",high
+#             return  jsonify(response="success",output=create_output(length))
+#         else:
+#             return jsonify(response="error", message="Not calculated yet please try again later")
+#     else:
+#             return jsonify(response="error",message="invalid reqeust")
 
 
 class get_fib(tornado.web.RequestHandler):
-    def get(self, id):
-        hello_world(id)
-        self.write(message)
+    @tornado.web.asynchronous
+    #@gen.engine
+    @gen.coroutine
+    def get(self, length):
+        result = yield self.hello_world(int(length))
+        result=tornado.escape.json_encode(result)
+        self.write(result)
+    @gen.coroutine
+    def message(self,length):
+        #text = 'message is',length
+        print "local_high:",local_high
+        hello_world(length)
+        raise gen.Return(str(text))
+
+    @gen.coroutine
+    def hello_world(self,length):
+        try:
+             length = int(length)
+        except:
+            return jsonify(response="error", message="invalid reqeust, reqeust must be an integer")
+
+        if (length > 0):
+            if ((length <= local_high) or ((length > local_high) and (rdb.exists(length)))):
+                #print "current high",high
+                result = self.create_output(length)
+                result = {'response':'success','output':result}
+                raise gen.Return(str(result))
+
+            else:
+                return {'response':'error', 'message':'Not calculated yet please try again later'}
+        else:
+                return jsonify(response="error",message="invalid reqeust")
+
+    @gen.coroutine
+    def create_output (self,length):
+            #This is the function that will walk the array and return the array
+            #for the given position
+        global local_high
+        print "local_high in output:",local_high
+        if length <= local_high:
+            fib_read.seek(0)
+            print "before read"
+            index=rdb.get('index_'+str(length))
+            print "index:",index
+            temp = yield fib_read.read(index)
+            print "temp:",temp
+            fib_output=yield fib_read.read(index)
+            print "after read"
+            yield fib_output
+            print "fib_output in output:",fib_output
+        else:
+            # If the length is more than we have in the cache file_name
+            # We need to update the cache file and then return the whole thing.
+            while (local_high < length):
+                fib_file.write((','+rdb.get(local_high+1)))
+                rdb.set('index_'+str(local_high+1),fib_file.tell())
+                local_high+=1
+
+            fib_file.flush()
+            fib_read.seek(0)
+            fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
+            #fib_output=fib_read.read()
+            #fib_output.extend(rdb.mget(range(max_array+1,length+1,1)))
+        yield fib_output
+        print "fib_output:",fib_output
+        raise gen.Return(str(fib_output))
+
 
 application = tornado.web.Application([
     (r"/fib/([0-9]+)",get_fib)
