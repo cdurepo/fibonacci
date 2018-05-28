1b42490cf47e29912156a9695be373fec249b368
Chris Durepo <chris.durepo@gmail.com>
2018-05-27 23:13:14 -0400

    Changed the local cache to be a file instead of the in memory array.

--- python/webservice.py
+++ python/webservice.py
@@ -15,14 +15,15 @@ from rediscluster import StrictRedisCluster
 
 
 file_name="/var/data/fib"
-os.remove(file_name)
-fib_file=open(file_name, 'a')
+#os.remove(file_name)
+fib_file=open(file_name, 'w')
 fib_read=open(file_name, 'r')
 file_index=0
 
 ### Setup
-max_array=1000
-max_redis=2000
+local_high=10
+max_array=10
+max_redis=20
 #Connect to redis
 
 rdb = redis.Redis(
@@ -41,58 +42,73 @@ print "1001:",value
 # Function to fill array
 def populate_fib_array ():
     # Changing to file
-    print "Populating array"
+    print "Populating array to ",max_array
     count=1
     first=0
     second=1
     #Bootstrap sequence
     file_index=fib_file.write(str(first))
+    print "file_index:",file_index
     rdb.set('index_'+str(count),fib_file.tell())
     file_index=fib_file.write(','+str(second))
     count+=1
     rdb.set('index_'+str(count),fib_file.tell())
     count+=1
-    while count < max_array:
+    while count <= max_array:
         third=first+second
-        print "thrid: ",third
+        print "adding:",third
         current=fib_file.tell()
         fib_file.write((','+str(third)))
-
         rdb.set('index_'+str(count),fib_file.tell())
-        print "index_"+str(count)," ",rdb.get('index_'+str(count))
+        rdb.set('redis_high',str(third))
         first=second
         second=third
-        fib_file.flush()
-        fib_read.seek(0)
-        #print "file count: ",fib_read.read()
-        #fib_array.append(fib_array[count-2]+fib_array[count-1])
-        #print "fib array count is:",fib_array[count]
         count+=1
+    fib_file.flush()
     return
 
-def populate_fib_redis (fib_one, fib_two):
+def populate_fib_redis ():
     print "Populating redis"
-    count=max_array+1
-    while count < max_redis:
+    count=1
+    redis_high=0
+    rdb.set('count','0')
+    count+=1
+    rdb.set('count','1')
+    count+=1
+    fib_one=0
+    fib_two=1
+    while count <= max_redis:
         value=fib_one+fib_two
         rdb.set(count,value)
+        rdb.set('redis_high',count)
         count+=1
+
         fib_one=fib_two
         fib_two=value
     return
 
-def create_output (length, fib_array):
+def create_output (length):
     #This is the function that will walk the array and return the array
     #for the given position
-    if length <= max_array:
+    global local_high
+    print "local_high:",local_high
+    if length <= local_high:
         fib_read.seek(0)
-        print "rdb output",rdb.get('index_'+str(length))
         fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
-        print "fib_output:",fib_output
     else:
-        #print "range",range(max_array+1,length+1,1)
-        fib_output=fib_array
-        fib_output.extend(rdb.mget(range(max_array+1,length+1,1)))
+        # If the length is more than we have in the cache file_name
+        # We need to update the cache file and then return the whole thing.
+        while (local_high <= length):
+            print "should be writing file"
+            fib_file.write((','+rdb.get(local_high+1)))
+            rdb.set('index_'+str(local_high+1),fib_file.tell())
+            local_high+=1
+
+        fib_file.flush()
+        fib_read.seek(0)
+        fib_output=fib_read.read(int(rdb.get('index_'+str(length))))
+        #fib_output=fib_read.read()
+        #fib_output.extend(rdb.mget(range(max_array+1,length+1,1)))
     return fib_output
 
 
@@ -103,10 +119,11 @@ print "Done filling array"
 #print "10:",create_output(10000, fib_array)
 print "Start filling redis"
 # Background redis population
-##fill_redis_thread = thread = threading.Thread(target=populate_fib_redis, args=(fib_array[-2], fib_array[-1]))
-##fill_redis_thread.start()
+fill_redis_thread = thread = threading.Thread(target=populate_fib_redis)
+fill_redis_thread.start()
 #populate_fib_redis(fib_array[-2], fib_array[-1])
 print "redis populated"
+create_output(1)
 
 #Building the webservice app
 app = Flask(__name__)
@@ -120,8 +137,9 @@ def hello_world():
         return jsonify(response="error", message="invalid reqeust, reqeust must be an integer")
 
     if (length > 0):
-        if ((length <= max_array) or ((length > max_array) and (rdb.exists(length)))):
-            return  jsonify(response="success",output=create_output(length, fib_array))
+        if ((length <= local_high) or ((length > local_high) and (rdb.exists(length)))):
+            #print "current high",high
+            return  jsonify(response="success",output=create_output(length))
         else:
             return jsonify(response="error", message="Not calculated yet please try again later")
     else:
